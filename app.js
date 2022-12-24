2ad21da7a9b2dc474412ee3eea2142c926725a38
Alberto-Knox <volkankarakaya2020@gmail.com>
2022-12-24 13:36:55 +0300

    add event listeners to change background of square divs

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 let mainContainer = document.getElementById("main-container");
-let squareNumber= 16;
-let squareSize = mainContainer.clientWidth / squareNumber
-
+let squareNumber = 30;
+let squareSize = mainContainer.clientWidth / squareNumber;
 
 for (let i = 0; i < squareNumber ** 2; i++) {
   let gridDiv = document.createElement("div");
   gridDiv.setAttribute("class", "square-grid");
   mainContainer.appendChild(gridDiv);
-  gridDiv.style.width = squareSize+'px'
-  gridDiv.style.height = squareSize+'px'
+  gridDiv.style.width = squareSize + "px";
+  gridDiv.style.height = squareSize + "px";
 }
 
 let squareDivs = document.querySelectorAll(".square-grid");
 
-squareDivs.forEach(item => {
-    item.addEventListener('mousedown', (e) =>{
+squareDivs.forEach((item) => {
+  item.addEventListener("mousedown", (e) => {
+    item.style.backgroundColor = "gray";
+    if (e.buttons == 1) {
 
-        item.style.backgroundColor = 'gray'
-        if (e.buttons==1){window.addEventListener('mouseover', ()=>{
-            console.log('holding click')
-        })}
-    })
-    
-})
+        squareDivs.forEach(div=>div.addEventListener("mouseover",(ev)=>{
+            if(ev.buttons==1){ev.target.style.backgroundColor = "black"}
+            
+        }))
+    //   window.addEventListener("mouseover", (ev) => {
+    //     if(ev.buttons==1){
+    //         ev.stopPropagation()
+    //         ev.target.style.backgroundColor = 'black'
+            
+    //     }
+        
+    //   });
+    }
+  });
+});
