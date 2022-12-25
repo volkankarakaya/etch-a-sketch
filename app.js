fa553d2b1f732672892fa7118c1a5d3fef334679
Alberto-Knox <volkankarakaya2020@gmail.com>
2022-12-25 13:22:22 +0300

    add prompt valid number checking feature

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 let mainContainer = document.getElementById("main-container");
-let squareNumber = 32;
+let squareNumber = Number(prompt("Choose grid size between 1-100!", "16"));
+
 let squareSize = mainContainer.clientWidth / squareNumber;
 
-console.log(squareSize, mainContainer.clientWidth)
+function checkValidPrompt(squareNumber) {
+  while (
+    squareNumber > 100 ||
+    squareNumber < 1 ||
+    !Number.isInteger(squareNumber)
+  ) {
+    squareNumber = Number(prompt("Enter a valid value!", "16"));
+  }
+}
+
+checkValidPrompt(squareNumber);
 
 for (let i = 0; i < squareNumber ** 2; i++) {
   let gridDiv = document.createElement("div");
@@ -19,19 +30,21 @@ squareDivs.forEach((item) => {
   item.addEventListener("mousedown", (e) => {
     item.style.backgroundColor = "gray";
     if (e.buttons == 1) {
+      squareDivs.forEach((div) =>
+        div.addEventListener("mouseover", (e) => {
+          if (e.buttons == 1) {
+            e.target.style.backgroundColor = "black";
+          }
+        })
+      );
+      //   window.addEventListener("mouseover", (ev) => {
+      //     if(ev.buttons==1){
+      //         ev.stopPropagation()
+      //         ev.target.style.backgroundColor = 'black'
 
-        squareDivs.forEach(div=>div.addEventListener("mouseover",(e)=>{
-            if(e.buttons==1){e.target.style.backgroundColor = "black"}
-            
-        }))
-    //   window.addEventListener("mouseover", (ev) => {
-    //     if(ev.buttons==1){
-    //         ev.stopPropagation()
-    //         ev.target.style.backgroundColor = 'black'
-            
-    //     }
-        
-    //   });
+      //     }
+
+      //   });
     }
   });
 });
